3531adb7401bc479de7f8728db53e24171ddd059
Wimpzer <firestorm1992@hotmail.com>
2015-03-03 11:06:48 +0100

    ...

--- src/field/Field.java
+++ src/field/Field.java
@@ -4,6 +4,8 @@ import interfaces.IField;
 
 public class Field implements IField{
 
+	private String name;
+	
 	@Override
 	public String getName() {
 		// TODO Auto-generated method stub
--- src/field/Ownable.java
+++ src/field/Ownable.java
@@ -1,5 +1,38 @@
 package field;
 
-public abstract class Ownable extends Field {
+import user.User;
 
+public abstract class Ownable extends Field {
+	protected int fieldPrice;
+	protected int rentPrice;
+	private User owner;
+
+	public Ownable(String name){
+		super(name);
+	}
+	
+	abstract public int rent();
+	
+	public void landOnField(User user){
+		if(getOwner() != null && getOwner() != user){
+			getOwner().deposit(rentPrice);
+			user.withdraw(rentPrice);
+		}else if(getOwner() == null){
+			setOwner(user);
+			user.withdraw(fieldPrice);
+		}
+	}
+	
+	public User getOwner(){
+		return owner;
+	}
+	
+	public void setOwner(User owner){
+		this.owner = owner;
+	}
+	
+	public int getFieldPrice(){
+		return fieldPrice;
+	}
+	
 }
