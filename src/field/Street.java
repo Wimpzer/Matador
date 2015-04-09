18c50e5c3f9fcf43f955b58d4a91290133449692
alstroem <alstroem1992@gmail.com>
2015-04-09 23:11:44 +0200

    Street

--- src/field/Street.java
+++ src/field/Street.java
@@ -5,17 +5,16 @@ import user.User;
 public class Street extends Ownable{
 	private String color;
 	
-	public Street(String name, int rentPrice, int fieldPrice, User owner, String color){
+	public Street(String name, int rentPrice, int fieldPrice, int userNumber, String color){
 		super(name);
 		this.rentPrice = rentPrice;
 		this.fieldPrice = fieldPrice;
-		this.setOwner(owner);
+		this.setOwner(userNumber);
 		this.color = color;
 	}
 
 	
 		// TODO Auto-generated constructor stub
-	}
 
 	@Override
 	public int rent() {
