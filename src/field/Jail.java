87a7a0e1e3aaab2b7a51a33198d1b060563b6f85
frederdes92 <fnoergaard92@gmail.com>
2015-04-14 13:36:03 +0200

    jail

--- src/field/Jail.java
+++ src/field/Jail.java
@@ -14,35 +14,35 @@ public class Jail extends Field {
 		Dice diceCup = new Dice();
 		int jailFine = 1000;
 		int rollCount = 0;
-		
+
 		if(user.getCurrentPosition() == 30){
+
 			if(user.getFreeJailCards() > 0){
 				user.setFreeJailCards(user.getFreeJailCards() - 1);
-				user.setInJail(false);
-			} else {
+				user.setCurrentPosition(10);
+			}else {
 				user.setInJail(true);
 				while(rollCount < 3)
 				{
 					diceCup.roll();
 					diceCup.checkEqual();
-					
+
 					if(diceCup.checkEqual() == false){
 						rollCount++;
 					} else {
 						user.setInJail(false);
 					}
-					if(rollCount == 3)
-					{
-						user.withdraw(jailFine);
-						user.setInJail(false);
-					}
-	
-					user.setCurrentPosition(10 + diceCup.getSum());
-					
 				}
+				if(rollCount == 3)
+				{
+					user.withdraw(jailFine);
+					user.setInJail(false);
+				}
+
+				user.setCurrentPosition(10 + diceCup.getSum());
+
 			}
 		}
-		
 	}
 
 }
