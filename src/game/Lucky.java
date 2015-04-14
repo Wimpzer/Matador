00e1dad848fdd81ea0bf0f299d5a7a1f6b4ad715
omarjuve <omar-juve@hotmail.com>
2015-04-14 16:49:31 +0200

    ..

--- src/game/Lucky.java
+++ src/game/Lucky.java
@@ -4,6 +4,10 @@ import user.User;
 import field.Field;
 
 public class Lucky extends Field {
+	public Lucky(int number, String name)
+	{
+		super(number, name);
+	}
 
 	@Override
 	public void landOnField(User user) {
